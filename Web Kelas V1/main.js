cec047c1ebfce5a0ad871f1ecf9729bbb7639426
AF <ahmadfagih.arrifai@gmail.com>
2024-11-17 20:06:00 +0700

    Add files via upload

---
+++ Web Kelas V1/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
